b81e6e94c2a604ed938721a0c2a9194e3b338946
corentin-roussel <corentin.roussel@laplateforme.io>
2024-01-03 23:39:28 +0100

    hangman with make file

--- hangman.c
+++ hangman.c
@@ -4,8 +4,8 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include "hangman.h"
+#include <time.h>
 #define MAXCHAR 100
 
 
@@ -26,8 +26,8 @@ char **openAndReadCsv(char *difficulty, char *fileName)
     }
 
 
-    char row[MAXCHAR];
-    char **arrayString = malloc(sizeof (char*) * sizeof row);
+    char row[256];
+    char **array = malloc(sizeof (char*));
     int i = 0;
 
 
@@ -35,33 +35,172 @@ char **openAndReadCsv(char *difficulty, char *fileName)
         if (strchr(row, '#') != NULL) {
             continue;
         }
-        if (strstr(row, difficulty) != NULL) {
-            arrayString = malloc(sizeof(char) * length(row) + 1);
-            arrayString[i] = row;
+        if(strstr(row, difficulty) != NULL) {
+            array = realloc(array, (i + 1) * sizeof(char *));
+
+            array[i] = malloc(strlen(row) + 1);
+
+            strcpy(array[i], row);
             i++;
         }
+        
     }
-    return arrayString;
+    array[i+1] = NULL;
+
+    fclose(inputFile);
+
+    return array;
+}
+
+void vider_buffer(void)
+{
+    int c;
+
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
 }
 
 char *chooseWord(char **strArray)
 {
-    int i = 0;
-    char *found;
-    printf("%s", *strArray);
+    srand(time(NULL));
 
+    char *found = malloc(sizeof(char*) * length(*strArray));
 
-    return found;
+    int random =  (rand() / (4 - 1 + 1)) +1 ;
+
+    int randomness= rand() / random;
+
+    strcpy(found, strArray[randomness]);
+
+    char **splitted = split(found);
+
+    return splitted[0];
 }
+
+int replay(void(*hangman)(char *dictionnary ,char *difficulty), char *dictionnary, char *difficulty, int gameStatus)
+{
+    char buffer;
+
+    printf("Do you want to replay : Y/N\n");
+    scanf("%c", &buffer);
+    vider_buffer();
+    if(buffer == 'Y') {
+        printf("Do you want to change difficulty ? Y/N\n");
+        scanf("%c", &buffer);
+        vider_buffer();
+        if (buffer == 'Y') {
+            printf("Choose between: facile, moyen, difficile\n");
+            scanf("%s", difficulty);
+            hangman(dictionnary, difficulty);
+        } else if(buffer == 'N'){
+            hangman(dictionnary, difficulty);
+        }else {
+            printf("Please choose the appropriate letter next time\n");
+            exit(0);
+        }
+    } else if(buffer == 'N'){
+        printf("Thanks for playing\n");
+        gameStatus = 0;
+    }else {
+        printf("Please choose the appropriate letter next time\n");
+        exit(0);
+    }
+    return gameStatus;
+}
+
+char *wordTransformed(char *stringTochange, char *emptyString)
+{
+    int i = 0;
+    while(i < length(stringTochange))
+    {
+        if(emptyString[i] == '-')
+        {
+            emptyString[i] = '-';
+            i++;
+        }
+        emptyString[i] = '*';
+        i++;
+    }
+    emptyString[i] = '\0';
+    return emptyString;
+}
+
+char askUser(int life, char *wordToFind, char buffer)
+{
+    printf("You have %d life\n", life);
+    printf("Word to find : %s\n", wordToFind);
+    printf("Please enter a letter: \n");
+    scanf("%c", &buffer);
+    vider_buffer();
+
+    return buffer;
+}
+
+
+void hangman(char *dictionnary, char *difficulty)
+{
+    int gameStatus = 1;
+    int life = 6;
+    char buffer = '\0';
+    int index = 0;
+    int fail = 0;
+    int gameWon = 0;
+
+    char **stringsFound = openAndReadCsv(difficulty, dictionnary);
+
+    char *wordToGuess = chooseWord(stringsFound);
+
+    char *wordToFind = malloc( sizeof(char) * length(wordToGuess));
+
+    wordToFind = wordTransformed(wordToGuess, wordToFind);
+
+    while(gameStatus)
+    {
+        buffer = askUser(life, wordToFind, buffer);
+
+        while(wordToFind[index] != '\0')
+        {
+            if(buffer == wordToGuess[index])
+            {
+                wordToFind[index] = buffer;
+                gameWon++;
+                fail++;
+            }
+
+            index++;
+        }
+        if(fail == 0)
+        {
+            life--;
+        }
+        fail = 0;
+        index = 0;
+
+        if(gameWon == length(wordToGuess))
+        {
+            printf("Congratulations you've won your word was: %s\n", wordToFind);
+            gameStatus = replay(hangman, dictionnary, difficulty, gameStatus);
+        }
+        if(life == 0)
+        {
+            printf("Ho no you lost your word was %s\n", wordToGuess);
+            gameStatus = replay(hangman, dictionnary, difficulty, gameStatus);
+        }
+    }
+}
+
+
+
 
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
+    if(argc < 3)
+    {
+        exit(-1);
+    }
 
-    char **stringsFound = openAndReadCsv("facile", "dictionnary.csv");
-    char *wordToGuess = chooseWord(stringsFound);
-
+    hangman(argv[1], argv[2]);
 
     return 0;
 }
--- makefile
+++ makefile
@@ -3,6 +3,13 @@ CFLAGS = -Wall -Wextra -Werror -g
 SRCS = hangman.c
 TARGET = hangman
 OBJS = $(SRCS:.c=.o)
+# If the first argument is "run"...
+ifeq (run,$(firstword $(MAKECMDGOALS)))
+  # use the rest as arguments for "run"
+  RUN_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+  # ...and turn them into do-nothing targets
+  $(eval $(RUN_ARGS):;@:)
+endif
 
 all: $(TARGET)
 
@@ -16,4 +23,4 @@ f-clean: clean
 	rm -f $(TARGET)
 
 run: $(TARGET)
-	./$<
+	./$< $(RUN_ARGS)
